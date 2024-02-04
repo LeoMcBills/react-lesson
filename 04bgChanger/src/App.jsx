3b67fbedc34bf588df6983a3ce2d2f53c6d2bfaf
LeoMcBills <leokinyera9@gmail.com>
2024-02-04 22:08:05 +0300

    background changer

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -11,20 +11,24 @@ function App() {
   return (
     <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+      <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
           <button
-          onClick={() => setColor('red')}
+          onClick={() => setColor('pink')}
           className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
-          style={{backgroundColor: 'red'}}
-          >Red</button>
+          style={{backgroundColor: 'pink'}}
+          >Pink</button>
           <button
           onClick={() => setColor('green')}
           className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
           style={{backgroundColor: 'green'}}
           >Green</button>
-          
-          </div>
+          <button
+          onClick={() => setColor('teal')}
+          className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+          style={{backgroundColor: 'teal'}}
+          >Teal</button>
       </div>
+      </div> 
     </div>
   )
 }
